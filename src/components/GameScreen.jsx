1d44eb694486b8c171bc121da896371eb8487174
sory <luizhmatoso1340@gmail.com>
2024-10-19 13:39:59 -0300

    :lipstick: feat: GameScreen styling

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -2,9 +2,32 @@ import styles from "./GameScreen.module.css";
 
 const GameScreen = ({ processInput }) => {
   return (
-    <div className={styles.gameScreen}>
-      <h1>Game</h1>
-      <button onClick={processInput}>Leave game</button>
+    <div className={styles.game}>
+      <p className={styles.points}>
+        <span>Pontuation: 000</span>
+      </p>
+      <h1>Do you know the word?</h1>
+      <h3 className={styles.tip}>
+        Word tip: <span>Tip......</span>
+      </h3>
+      <p>You stiil have XXX attemps</p>
+      <div className={styles.word_container}>
+        <span className={styles.letter}>A</span>
+        <span className={styles.blank_square}></span>
+      </div>
+      <div className={styles.letter_container}>
+        <p>Guess the word letter: </p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Guess</button>
+        </form>
+      </div>
+      <div className={styles.wrong_letter_container}>
+        <p>Used letters:</p>
+        <span>a, </span>
+        <span>b, </span>
+        <span>c, </span>
+      </div>
     </div>
   );
 };
